6fa089689bc41d54f9c89761540bad2c0fc8445f
myatpwintphyu <myatpwintphyu.pk64@gmail.com>
2024-08-06 22:08:24 +0630

    Second commit

--- db.sql
+++ db.sql
@@ -1,3 +1,13 @@
+CREATE TABLE employees (
+    employee_id INT PRIMARY KEY,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    email VARCHAR(100),
+    hire_date DATE,
+    salary DECIMAL(10, 2)
+);
+
+
 INSERT INTO employees (employee_id, first_name, last_name, email, hire_date, salary)
 VALUES
     (1, 'John', 'Doe', 'john.doe@example.com', '2023-01-15', 60000.00),
