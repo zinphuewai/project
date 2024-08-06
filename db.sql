e9962079bf5f29e6be838052f68d4c0c8920d4c0
zinphuewai <zinphuewai53@gmail.com>
2024-08-06 21:59:00 +0630

    first commit

---
+++ db.sql
@@ -0,0 +1,7 @@
+INSERT INTO employees (employee_id, first_name, last_name, email, hire_date, salary)
+VALUES
+    (1, 'John', 'Doe', 'john.doe@example.com', '2023-01-15', 60000.00),
+    (2, 'Jane', 'Smith', 'jane.smith@example.com', '2022-05-23', 75000.00),
+    (3, 'Michael', 'Brown', 'michael.brown@example.com', '2024-03-10', 82000.00),
+    (4, 'Emily', 'Davis', 'emily.davis@example.com', '2021-08-17', 68000.00),
+    (5, 'David', 'Wilson', 'david.wilson@example.com', '2020-12-30', 72000.00);
